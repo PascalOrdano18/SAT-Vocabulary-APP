81645a867aef2c7026f6f7fa97809469a2f62711
Pascal Ordano <pascalordanodev@gmail.com>
2022-08-22 15:26:43 -0300

    new color change

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -14,11 +14,11 @@ const index = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <div className="w-screen h-screen flex flex-col bg-red-900">
+      <div className="w-screen h-screen flex flex-col bg-blue-900">
 
         <div className="mx-auto mt-20 max-w-[740px]">
             <h1 className="mx-auto text-6xl w-fit underline">Multiple Choice Quiz</h1>
-            <p className="w-fit mx-auto my-16 text-xl">A definition will be given with four possible corresponding words. Only one word corresponds with the definition displayed. Good Luck</p>
+            <p className="w-fit mx-auto my-16 text-xl">A definition will be given with four possible corresponding words, but only one word is the correct answer. Good Luck</p>
         </div>
 
 
